cbf2924ef73f799f5bdf298d0f09db7e0105c8a8
Polunina Svetlana <andj.sv@gmail.com>
2024-10-10 01:18:39 +0300

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import globals from 'globals';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended,
+});
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
+        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  },
+  ...compat.extends('airbnb-base'),
+  {
+    rules: {
+      'no-console': 'off',
+      'import/extensions': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+    },
+  },
+];
