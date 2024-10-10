62bda87589f9602577eb709d7a797b42a244c1ec
Polunina Svetlana <andj.sv@gmail.com>
2024-10-10 17:59:13 +0300

    fix export greeting to default

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
-/* eslint import/prefer-default-export: 'off' */
-
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
+
+export default greeting;
