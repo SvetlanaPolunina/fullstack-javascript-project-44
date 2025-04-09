bf905a252db2d66e7fc5b36fc556debf0e6e5b5c
Polunina Svetlana <andj.sv@gmail.com>
2025-04-09 20:24:12 +0300

    add asking user name to scr/cli.js

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSyng from 'readline-sync';
+
+const askUserName = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSyng.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+
+export { askUserName };
