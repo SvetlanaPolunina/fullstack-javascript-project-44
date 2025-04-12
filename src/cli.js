bc7ee0b8d681c0ebee35f3b91fe5f7641b24693c
Polunina Svetlana <andj.sv@gmail.com>
2025-04-13 02:35:57 +0300

    add triming asked name at askUserName in src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,7 @@ import readlineSyng from 'readline-sync';
 
 const askUserName = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSyng.question('May I have your name? ');
+  const name = readlineSyng.question('May I have your name? ').trim();
   console.log(`Hello, ${name}!`);
   return name;
 };
