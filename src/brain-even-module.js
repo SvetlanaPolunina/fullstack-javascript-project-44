3aad7ce661b9d1dec776cbd0d634df74ab1fe28d
Polunina Svetlana <andj.sv@gmail.com>
2025-04-13 02:28:18 +0300

    add generation of quiz to brain-even-module

--- src/brain-even-module.js
+++ src/brain-even-module.js
@@ -2,33 +2,52 @@ import readlineSyng from 'readline-sync';
 import { askUserName } from './cli.js';
 
 const brainEven = () => {
-  const roundCount = 3;
-  const quiz = [
-    { question: 1, correctAnswer: 'no' },
-    { question: 2, correctAnswer: 'yes' },
-    { question: 3, correctAnswer: 'no' },
-  ];
-  const name = askUserName();
+  const getBrainEvenQuiz = (roundCount, maxNum) => {
+    const getRandomNum = (max) => Math.floor(Math.random() * max);
+    const isEven = (num) => num % 2 === 0;
+    const getCorrectAnswer = (num) => (isEven(num) ? 'yes' : 'no');
 
-  console.log('Answer "yes" if the number is even, otherwise answer "no"');
-
-  for (let i = 0; i < roundCount; i += 1) {
-    const { question, correctAnswer } = quiz.at(i);
-
-    console.log(`Question: ${question}`);
-    const answer = readlineSyng.question('Your answer: ');
-
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-      console.log(`Let's try again, ${name}!`);
-      return 0;
+    const quiz = [];
+    for (let i = 0; i < roundCount; i += 1) {
+      const question = getRandomNum(maxNum);
+      const correctAnswer = getCorrectAnswer(question);
+      const round = { question, correctAnswer };
+      quiz.push(round);
     }
-  }
 
-  console.log(`Congratulations, ${name}!`);
-  return 0;
+    return quiz;
+  };
+
+  const runQuiz = (quiz, name, gameConditionsMessage) => {
+    const roundCount = quiz.length;
+    console.log(gameConditionsMessage);
+
+    for (let i = 0; i < roundCount; i += 1) {
+      const { question, correctAnswer } = quiz.at(i);
+
+      console.log(`Question: ${question}`);
+      const answer = readlineSyng.question('Your answer: ').trim();
+
+      if (answer === correctAnswer) {
+        console.log('Correct!');
+      } else {
+        console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+        console.log(`Let's try again, ${name}!`);
+        return 0;
+      }
+    }
+
+    console.log(`Congratulations, ${name}!`);
+    return 0;
+  };
+
+  const roundCount = 3;
+  const maxNum = 100;
+  const gameConditionsMessage = 'Answer "yes" if the number is even, otherwise answer "no"';
+
+  const brainEvenQuiz = getBrainEvenQuiz(roundCount, maxNum);
+  const name = askUserName();
+  runQuiz(brainEvenQuiz, name, gameConditionsMessage);
 };
 
 export default brainEven;
