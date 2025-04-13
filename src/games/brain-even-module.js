14c18e76d8479e6b27bd45aa8348b1457e48b05a
Polunina Svetlana <andj.sv@gmail.com>
2025-04-13 06:21:14 +0300

    fix brain-even-module.js using common logic in src/index.js

--- src/games/brain-even-module.js
+++ src/games/brain-even-module.js
@@ -1,5 +1,4 @@
-import readlineSyng from 'readline-sync';
-import askUserName from '../cli.js';
+import runQuiz from '../index.js';
 
 const brainEven = () => {
   const getBrainEvenQuiz = (roundCount, maxNum) => {
@@ -18,36 +17,12 @@ const brainEven = () => {
     return quiz;
   };
 
-  const runQuiz = (quiz, name, gameConditionsMessage) => {
-    const roundCount = quiz.length;
-    console.log(gameConditionsMessage);
-
-    for (let i = 0; i < roundCount; i += 1) {
-      const { question, correctAnswer } = quiz.at(i);
-
-      console.log(`Question: ${question}`);
-      const answer = readlineSyng.question('Your answer: ').trim();
-
-      if (answer === correctAnswer) {
-        console.log('Correct!');
-      } else {
-        console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-        console.log(`Let's try again, ${name}!`);
-        return 0;
-      }
-    }
-
-    console.log(`Congratulations, ${name}!`);
-    return 0;
-  };
-
   const roundCount = 3;
   const maxNum = 100;
   const gameConditionsMessage = 'Answer "yes" if the number is even, otherwise answer "no"';
 
   const brainEvenQuiz = getBrainEvenQuiz(roundCount, maxNum);
-  const name = askUserName();
-  runQuiz(brainEvenQuiz, name, gameConditionsMessage);
+  runQuiz(brainEvenQuiz, gameConditionsMessage);
 };
 
 export default brainEven;
