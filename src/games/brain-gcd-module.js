c7ed84837ef5ef3b13f85b6f8521942ee78473c4
Polunina Svetlana <andj.sv@gmail.com>
2025-04-13 20:07:31 +0300

    story part of work in brain-gcd-module.js

--- src/games/brain-gcd-module.js
+++ src/games/brain-gcd-module.js
@@ -1,5 +1,27 @@
-const brainGsd = () => {
+import runQuiz from '../index.js';
 
+const brainGsd = () => {
+  const getRaund = () => {
+
+  };
+
+  const getQuiz = (gameCondition) => {
+    const roundCount = 3;
+
+    const quiz = [];
+    for (let i = 0; i < roundCount; i += 1) {
+      const round = getRaund();
+      quiz.push(round);
+    }
+
+    return quiz;
+  };
+
+  // quiz = getQuiz();
+  const quiz = [];
+  const gameConditionsMessage = 'Find the greatest common divisor of given numbers.';
+
+  runQuiz(quiz, gameConditionsMessage);
 };
 
 export default brainGsd;
